a9eb2aedd57e52c0bddceae03a59e55a1a749bab
skarfroth <mikko.raussi1@gmail.com>
2022-06-06 20:35:47 +0300

    actual first commit

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,24 @@
+using Mirror;
+using Steamworks;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomNetworkManager : NetworkManager
+{
+    [SerializeField] private PlayerObjectController GamePlayerPrefab;
+    public List<PlayerObjectController> GamePlayers { get; } = new List<PlayerObjectController>();
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        if (SceneManager.GetActiveScene().name == "Lobby")
+        {
+            PlayerObjectController GamePlayerInstance = Instantiate(GamePlayerPrefab);
+            GamePlayerInstance.ConnectionID = conn.connectionId;
+            GamePlayerInstance.PlayerIdNumber = GamePlayers.Count + 1;
+            GamePlayerInstance.PlayerSteamID = (ulong)SteamMatchmaking.GetLobbyMemberByIndex((CSteamID)SteamLobby.Instance.CurrentLobbyID, GamePlayers.Count);
+
+            NetworkServer.AddPlayerForConnection(conn, GamePlayerInstance.gameObject);
+        }
+    }
+}
